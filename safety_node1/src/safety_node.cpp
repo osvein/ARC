64efe4f6e84406e6ca366121b47fd6fc67781d0f
JoLo <48959849+GeorgeJoLo@users.noreply.github.com>
2022-03-28 19:48:02 +0200

    C++ node working

--- safety_node1/src/safety_node.cpp
+++ safety_node1/src/safety_node.cpp
@@ -1,16 +1,16 @@
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/LaserScan.h>
-// TODO: include ROS msg type headers and libraries
-// Publish to a topic with this message type
 #include <ackermann_msgs/AckermannDriveStamped.h>
 #include "std_msgs/Bool.h"
 #include "std_msgs/Float32.h"
 #include <cmath>
 
 #define SCAN_BEAMS 1080
-#define MAX_DECEL 8.26
-#define MIN_TTC 1.0
+#define MIN_TTC 0.3
+#define BIG_TTC 1000.0
+#define SCAN_FIELD_OF_VIEW 4.71
+
 
 
 class Safety {
@@ -18,35 +18,33 @@ class Safety {
 private:
     ros::NodeHandle n;
     double speed;
-    int scan_beams;
+
+    // Auxiliary variables for monitoring
     double max_decel, min_ttc;
 
 
-    // TODO: create ROS subscribers and publishers
     // Listen for scan messages
     ros::Subscriber scan_sub;
-
     // Listen for odom messages
     ros::Subscriber odom_sub;
 
     // Publish braking data
     ros::Publisher brake_pub;
-
     // Publish bool data
     ros::Publisher bool_pub;
   
-
+    // Auxiliary publishers for monitoring
     ros::Publisher minDist_pub;
 
 
     double beam_angle(int beam) {
-	return -3.14159274101 + beam * 6.28318548202 / scan_beams;
+		return -SCAN_FIELD_OF_VIEW/2 + beam * SCAN_FIELD_OF_VIEW / SCAN_BEAMS;
     }
 
 
 public:
     Safety() {
-        n = ros::NodeHandle("~");
+        n = ros::NodeHandle();
         speed = 0.0;
         /*
         One publisher should publish to the /brake topic with an
@@ -64,81 +62,66 @@ public:
 
         NOTE that the x component of the linear velocity in odom is the speed
         */
-	
-	//get params
-	n.getParam("scan_beams", scan_beams);
-	n.getParam("max_decel", max_decel);
-	n.getParam("min_ttc", min_ttc);
 
-	std::cout << min_ttc << " <----------------------------dsafdsf-sad-f-adsf-ads-f-a" << std::endl;
-
-        // TODO: create ROS subscribers and publishers
         // Start a subscriber to listen to odom messages
         odom_sub = n.subscribe("/odom", 1, &Safety::odom_callback, this);
-
         // Start a subscriber to listen to scan messages
         scan_sub = n.subscribe("/scan", 1, &Safety::scan_callback, this);
 
         // Make a publisher for brakeing messages
         brake_pub = n.advertise<ackermann_msgs::AckermannDriveStamped>("/brake", 10);
-
         // Make a publisher for bool messages
         bool_pub = n.advertise<std_msgs::Bool>("/brake_bool", 10);
 
-        // Make a publisher for bool messages
+        // Make auxiliary publisher for monitoring distance
         minDist_pub = n.advertise<std_msgs::Float32>("/min_dist", 10);
     }
 
     void odom_callback(const nav_msgs::Odometry::ConstPtr &odom_msg) {
-        // TODO: update current speed
         speed = odom_msg->twist.twist.linear.x;
     }
 
     void scan_callback(const sensor_msgs::LaserScan::ConstPtr &scan_msg) {
-        // TODO: calculate TTC
-	double r_dot[scan_beams];
-	double TTCs[scan_beams];
-	bool must_brake = false;
-	
-	for (int i = 0; i < scan_beams; i++)
-		r_dot[i] = speed * cos(beam_angle(i))>0?speed * cos(beam_angle(i)) : 0;
+		double r_dot[SCAN_BEAMS];
+		double TTCs[SCAN_BEAMS];
+		bool must_brake = false;
 
-	for (int i = 0; i < scan_beams; i++)
-		if (r_dot[i] > 0) {
-			TTCs[i] = scan_msg->ranges[i] / r_dot[i];
-			if (TTCs[i] < min_ttc) 
-				must_brake = true;
+        // calculate speed for each beam angle  
+		for (int i = 0; i < SCAN_BEAMS; i++)
+			r_dot[i] = speed * cos(beam_angle(i))>0?speed * cos(beam_angle(i)) : 0;
+
+		// calculate TTC for each beam
+		for (int i = 0; i < SCAN_BEAMS; i++)
+			if (r_dot[i] > 0) {
+				TTCs[i] = scan_msg->ranges[i] / r_dot[i];
+				if (TTCs[i] < MIN_TTC) 
+					must_brake = true;
+			}
+			else 
+				TTCs[i] = BIG_TTC;
+
+	
+		// Find closest distance
+		double mn_dist = 1000.0;
+		for (int i = 0; i < SCAN_BEAMS; i++)
+			if (scan_msg->ranges[i] < mn_dist)
+				mn_dist = scan_msg->ranges[i];
+
+    	// initialize message to be published
+    	ackermann_msgs::AckermannDriveStamped ack_msg;
+		ack_msg.drive.speed = 0.0;
+
+		std_msgs::Bool braking_msg;
+		braking_msg.data = must_brake;
+	
+		bool_pub.publish(braking_msg);
+		if (must_brake) {
+        	brake_pub.publish(ack_msg);
 		}
-		else 
-			TTCs[i] = 1000.0;
 
-	
-	// Find closest distance
-	double mn_dist = 1000.0;
-	for (int i = 0; i < scan_beams; i++)
-		if (scan_msg->ranges[i] < mn_dist)
-			mn_dist = scan_msg->ranges[i];
-
-
-        // TODO: publish drive/brake message
-        // initialize message to be published
-        ackermann_msgs::AckermannDriveStamped drive_st_msg;
-	drive_st_msg.drive.speed = 0.0;
-
-	std_msgs::Bool braking;
-	braking.data = true;
-	
-	if (must_brake) {
-        // publish AckermannDriveStamped message to drive topic
-        brake_pub.publish(drive_st_msg);
-	bool_pub.publish(braking);}
-
-
-
-
-	std_msgs::Float32 mn;
-	mn.data = mn_dist;
-	minDist_pub.publish(mn);
+		std_msgs::Float32 mn;
+		mn.data = mn_dist;
+		minDist_pub.publish(mn);
     }
 
 };
